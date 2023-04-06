3607e505da45398e346df6cf3513599ef8852994
ghiza <ghizasanmatei15@gmail.com>
2023-04-06 20:28:40 +0300

    Setup in continuare

---
+++ ProiectWeb/Controllers/ItemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProiectWeb.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProiectWebService/Services/ItemsDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectWebService.Services
+{
+    public class ItemsDto
+    {
+        public int ItemId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Description { get; set; }
+
+        public float Price { get; set; }
+
+        public byte[] ThumbNail { get; set; }
+
+        public byte Image { get; set; }
+
+        public DateTime DateOfCreation { get; set; } = DateTime.Now;
+
+        public bool IsPromoted { get; set; }
+
+        public bool IsPopular { get; set; }
+    }
+}
